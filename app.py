523df0e97e5655845822b771d503d48c17c4e931
Johannes Alneberg <johannesalneberg@gmail.com>
2015-12-15 17:42:02 +0100

    Imported all models to app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
+from models import Sample, SampleSet, TimePlace, SampleProperty
+
 @app.route('/')
 def hello():
     return "Hello World!"
