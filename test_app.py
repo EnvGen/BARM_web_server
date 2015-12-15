93f6d2c447a628f80d14d379d1c50f29e7bddf97
Johannes Alneberg <johannesalneberg@gmail.com>
2015-12-15 15:55:59 +0100

    Tests for sample and sample set relations

--- test_app.py
+++ test_app.py
@@ -37,3 +37,35 @@ class SampleTestCase(unittest.TestCase):
         all_samples_after = Sample.query.all()
         assert len(all_samples_after) == 1
 
+    def test_sample_sampleset(self):
+        #Test that sample and sample set references each other properly
+        sample_set = SampleSet("first_sampleset")
+        sample1 = Sample("P1993_101", sample_set)
+
+        self.session.add(sample1)
+        self.session.add(sample_set)
+
+        # Sample sets relations are created
+        assert Sample.query.filter_by(scilifelab_code='P1993_101').first().sample_set is sample_set
+
+        sample_set2 = SampleSet("second_sampleset")
+        sample2 = Sample("P1993_102", sample_set2)
+        sample3 = Sample("P1993_103", sample_set)
+        self.session.add(sample2)
+        self.session.add(sample3)
+
+        self.session.commit()
+
+        # Sample2 should have sample set 2
+        assert Sample.query.filter_by(scilifelab_code='P1993_102').first().sample_set is sample_set2
+
+        # There should be 2 sample sets and 3 samples in the db
+        assert len(SampleSet.query.all()) == 2
+        assert len(Sample.query.all()) == 3
+
+        # The reverse relationship for sample set 2 should have sample1 and sample3
+        assert len(sample_set.samples) == 2
+
+        assert sample2 not in sample_set.samples
+        assert sample1 in sample_set.samples
+        assert sample3 in sample_set.samples
