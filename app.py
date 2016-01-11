ca462bec490a3e6948edc91e52b2678058711105
Johannes Alneberg <johannesalneberg@gmail.com>
2016-01-11 16:48:00 +0100

    Filter rpkm table on function class

--- app.py
+++ app.py
@@ -20,7 +20,20 @@ def function_classes_table():
     if button_id != '' and button_id.startswith('only_'):
         function_class = button_id.replace('only_', '')
         function_class = function_class.lower()
-    samples, table = Annotation.rpkm_table(function_class=function_class)
+    else:
+        function_class = None
+    limit = request.args.get('limit', '')
+    if limit == 'all':
+        limit = None
+    elif limit == '':
+        limit = 20
+    else:
+        try:
+            limit = int(limit)
+        except:
+            raise Exception
+    print(limit)
+    samples, table = Annotation.rpkm_table(limit=limit, function_class=function_class)
     return render_template('function_classes_table.html', table=table, samples=samples, button_id=button_id)
 
 if __name__ == '__main__':
