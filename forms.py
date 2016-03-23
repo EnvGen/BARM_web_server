ada9cb6fbe124fcab88ef96da6e0c2411b793e39
Johannes Alneberg <johannesalneberg@gmail.com>
2016-03-23 16:09:18 +0100

    Change the order of the filtering options

--- forms.py
+++ forms.py
@@ -6,8 +6,8 @@ class FunctionClassFilterForm(Form):
     function_class = SelectField(u'Function Classes', default='all')
     limit = SelectField(u'Limit', choices=[('10','10'), ('20', '20'), ('50', '50'), ('100', '100'), ('all', 'Show All')], default='20')
     filter_alternative = RadioField(choices=[
-      ('filter_with_type_identifiers', 'Filter by typing in individual annotation identifiers'),
-      ('filter_with_search', 'Filter by a search term')],
+      ('filter_with_search', 'Filter by a search term'),
+      ('filter_with_type_identifiers', 'Filter by typing in individual annotation identifiers')],
       default='filter_with_type_identifiers')
     type_identifiers = FieldList(StringField(u'Type identifier'))
     search_annotations = StringField('Search Annotations')
