ba6055fa73e1bc327bf6180de5f5a3771a227720
Johannes Alneberg <johannesalneberg@gmail.com>
2016-03-30 13:52:14 +0200

    First real integration tests with splinter

--- test_integration.py
+++ test_integration.py
@@ -1,6 +1,12 @@
 import unittest
 import urllib
 import app
+import splinter
+from splinter import Browser
+
+import sys
+
+import time
 
 class SampleTestCase(unittest.TestCase):
     """Test that the website shows the proper things"""
@@ -19,3 +25,62 @@ class SampleTestCase(unittest.TestCase):
         assert b'Baltic Sea Reference Metagenome' in r.data
 
         assert b'Function Classes' in r.data
+
+    def test_filtering_search(self):
+        with Browser() as browser:
+            url = "http://localhost:5000/"
+            browser.visit(url)
+
+            # Verify the accordion unfolding
+            filter_search_text = "Filter by a search term"
+            assert browser.is_text_not_present(filter_search_text)
+
+            browser.find_by_id("filter_accordion").click()
+
+            time.sleep(0.3) # The accordion takes some time to unfold
+
+            assert browser.is_text_present(filter_search_text)
+
+            # Verify the search term filtering
+            browser.find_by_value("filter_with_search").first.click()
+            browser.fill('search_annotations', "octaprenyltransferase")
+
+            browser.execute_script("window.scrollTo(0,1000)")
+            time.sleep(4) # wait for search result to load
+            assert browser.is_text_present("Showing 4 out of 4 in total")
+
+            browser.find_by_id('submit_filter').click()
+
+            time.sleep(2) # wait for page to reload with new result
+            # There are only two of these which is present as annotations
+            # in the test result
+            assert len(browser.find_by_tag('tr')) == 3 # only showing the filtered rows
+
+    def test_filtering_type_identifier(self):
+        with Browser() as browser:
+            url = "http://localhost:5000/"
+            browser.visit(url)
+
+            browser.find_by_id("filter_accordion").click()
+            time.sleep(1) # The accordion takes some time to unfold
+
+            # Verify the type identifiers filtering
+            browser.find_by_value("filter_with_type_identifiers").click()
+            browser.fill('type_identifiers-0', 'pfam01027')
+
+            browser.find_by_id('submit_filter').click()
+            assert browser.is_text_present("pfam01027")
+            assert len(browser.find_by_tag('tr')) == 2
+
+            browser.find_by_id("filter_accordion").click()
+            time.sleep(1) # The accordion takes some time to unfold
+
+            with self.assertRaises(splinter.exceptions.ElementDoesNotExist):
+                browser.fill('type_identifiers-1', 'TIGR01320')
+            browser.find_by_id('AddAnotherTypeIdentifier').click()
+            browser.fill('type_identifiers-1', 'TIGR03634')
+
+            browser.find_by_id('submit_filter').click()
+            assert "pfam01027" in browser.find_by_tag('table').first.html
+            assert "TIGR03634" in browser.find_by_tag('table').first.html
+            assert len(browser.find_by_tag('tr')) == 3
