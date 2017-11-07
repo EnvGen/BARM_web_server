344992f6b5a9f626a40baefb39db85f417f936d4
Johannes Alneberg <johannesalneberg@gmail.com>
2017-11-07 15:11:47 +0100

    Add more variables in local variables for devel use

--- local_variables.sh
+++ local_variables.sh
@@ -2,3 +2,5 @@
 
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL="postgresql://$USER:local_dev_pass@localhost/barm_web_dev"
+export OAUTHLIB_INSECURE_TRANSPORT=1
+export OAUTHLIB_RELAX_TOKEN_SCOPE=1
