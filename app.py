6117c3e2ced9c466459beefd7a99ab6b2fbf2db6
Johannes Alneberg <johannesalneberg@gmail.com>
2018-03-06 17:41:34 +0100

    First try with using max value for tax highcharts

--- app.py
+++ app.py
@@ -149,13 +149,18 @@ def taxon_tree_table_row(level, complete_taxonomy):
 
     rpkm_row = Taxon.rpkm_table_row(level, complete_taxonomy)
     rpkm_row['complete_taxonomy_id'] = complete_taxonomy.replace(';','-').replace(' ', '_').replace('.','_')
+    rpkm_row['highcharts_max_val'] = {}
 
     json_table = {}
     json_table[complete_taxonomy] = {}
     for sample_set, samples in sample_sets.items():
         json_table_row = []
+        ymax = 0
         for sample in samples:
             json_table_row.append({'y': rpkm_row[sample], 'sample': sample.scilifelab_code})
+            if rpkm_row[sample] > ymax:
+                ymax = rpkm_row[sample]
+        rpkm_row['highcharts_max_val'][sample_set.name] = "{0:.2E}".format(ymax)
         json_table[complete_taxonomy][sample_set.name] = json_table_row
 
     return render_template('taxon_tree_table_row.html',
@@ -198,10 +203,15 @@ def taxonomy_tree_table():
         complete_val_to_val[complete_taxonomy] = taxa_name
 
         table_row = Taxon.rpkm_table_row(complete_taxonomy=complete_taxonomy)
+        table_row['highcharts_max_val'] = {}
         for sample_set, samples in sample_sets.items():
             json_table_row = []
+            ymax = 0
             for sample in samples:
                 json_table_row.append({'y': table_row[sample], 'sample': sample.scilifelab_code})
+                if table_row[sample] > ymax:
+                    ymax = table_row[sample]
+            table_row['highcharts_max_val'][sample_set.name] = "{0:.2E}".format(ymax)
             json_table[complete_taxonomy][sample_set.name] = json_table_row
 
         table_row['complete_taxonomy_id'] = complete_taxonomy.replace(';','-').replace(' ', '_').replace('.','_')
